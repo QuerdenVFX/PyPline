65a0d90e092a2c873bddebc9f9c9a1c757c53084
QuerdenVFX <gautier.ray.vfx@gmail.com>
2024-10-04 18:04:44 +0200

    Test PyLint to correct code

--- funct/help.py
+++ funct/help.py
@@ -1,55 +1,55 @@
 import csv
-from rich import print
-from funct.frame import create_frame
 import os
 import subprocess as sp
 
+# from rich import print
 
-class API():
-    
-        commands = {
+from funct.frame import create_frame
+
+
+class API:
+
+    commands = {
         "help": "Show help",
         "add": "Add new project or shot",
         "conf": "Configure software list",
         "list": "List all project",
-        "go": "Run software with selected project and shot", 
+        "go": "Run software with selected project and shot",
         "clear": "Clear the command line",
         "reload": "Reload scripts",
-        "reset":"Reset work environment",
-        "exit": "Close PyPline"
+        "reset": "Reset work environment",
+        "exit": "Close PyPline",
     }
-        software_paths = {}
-        software_ext = {}
-        help = []
-        software_dict = []
+    software_paths = {}
+    software_ext = {}
+    help = []
+    software_dict = []
 
+    file = os.path.abspath("config.csv")
+    if os.path.isfile(file):
+        with open(file, mode="r", encoding="UTF-8") as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                if row["path"] != "#    Path not Defined    #":
+                    software = row["software"]
+                    commands[software] = f"Execute le programme {software}"
+                    software_paths[software] = row["path"]
+                    software_dict.append(software)
+                    software_ext[software] = row["extension"]
 
-        file = os.path.abspath("config.csv")
-        if os.path.isfile(file):
-            with open(file, mode="r") as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    if row["path"] != "#    Path not Defined    #":
-                        software = row['software']
-                        commands[software] = f"Execute le programme {software}"
-                        software_paths[software] = row["path"]
-                        software_dict.append(software)
-                        software_ext[software] = row["extension"]
+    else:
+        create_frame(["config.csv missing", "new config.csv file created"])
+        data = ["software", "path", "matrices", "extension"]
+        with open(file, mode="w", newline="", encoding="UTF-8") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(data)
 
-        else:
-            create_frame(['config.csv missing', 'new config.csv file created'])
-            data = ['software','path','matrices','extension']
-            with open(file, mode="w", newline="") as csv_file:
-                writer = csv.writer(csv_file)
-                writer.writerow(data)
-
-        for command, description in commands.items():
-                help.append(f"   {command}: {description}    ")
+    for command, description in commands.items():
+        help.append(f"   {command}: {description}    ")
 
 
 def commandList():
     return API.commands
-
 
 
 def showHelp():
@@ -57,6 +57,6 @@ def showHelp():
 
 
 def testHelp(soft):
-     if soft in API.software_dict:
+    if soft in API.software_dict:
         sp.Popen(API.software_paths[soft])
-          #print(f"{soft} : {ConfigCSV.software_paths[soft]}")
+        # print(f"{soft} : {ConfigCSV.software_paths[soft]}")
